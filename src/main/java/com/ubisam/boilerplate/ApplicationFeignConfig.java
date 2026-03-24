4878dedce8c804324767de21dff0d4ee4da251e6
조흥재 연구원 <heung@ubisam.com>
2026-03-24 17:45:07 +0900

    20260324-feignConfig 추가

---
+++ src/main/java/com/ubisam/boilerplate/ApplicationFeignConfig.java
@@ -0,0 +1,10 @@
+package com.ubisam.boilerplate;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class ApplicationFeignConfig {
+  
+}
